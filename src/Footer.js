2a85239ff74bff7d92d6ed59ff8fa05cbdd231b1
anishsinhaa <anishsinha491@gmail.com>
2023-05-10 18:48:25 +0530

    2nd commit

--- src/Footer.js
+++ src/Footer.js
@@ -4,15 +4,15 @@ import tw from "./images/twitter.png";
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="https://www.google.com">
+      <a href="https://github.com/anishsinhaa">
         <img src={gh} alt="GitHub" width="20px" />
       </a>
 
-      <a href="https://www.google.com">
+      <a href="https://leetcode.com/anishsinhaa/">
         <img src={lc} alt="LeetCode" width="20px" />
       </a>
 
-      <a href="https://www.google.com">
+      <a href="https://twitter.com/_anishsinha_">
         <img src={tw} alt="Twitter" width="20px" />
       </a>
     </footer>
